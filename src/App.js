84e1e43ad995254558a155c7e7efebb6754b9b9d
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-05-06 15:58:04 -0700

    Initial state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React, { useState, useEffect } from 'react'
-import {
-  FaEnvelopeOpen,
-  FaUser,
-  FaCalendarTimes,
-  FaMap,
-  FaPhone,
-  FaLock,
-} from 'react-icons/fa'
-const url = 'https://randomuser.me/api/'
-const defaultImage = 'https://randomuser.me/api/portraits/men/75.jpg'
-function App() {
-  return <h2>random user starter</h2>
+import React, { useState, useEffect } from 'react';
+import { FaEnvelopeOpen, FaUser, FaCalendarTimes, FaMap, FaPhone, FaLock } from 'react-icons/fa';
+const url = 'https://randomuser.me/api/';
+const defaultImage = 'https://randomuser.me/api/portraits/men/75.jpg';
+function App () {
+	const [ loading, setLoading ] = useState(true);
+	const [ person, setPerson ] = useState(null);
+	const [ title, setTitle ] = useState('name');
+	const [ value, setValue ] = useState('random person');
+
+	const handleValue = (e) => {
+		console.log(e.target);
+	};
+
+	return <h2>random user starter</h2>;
 }
 
-export default App
+export default App;
