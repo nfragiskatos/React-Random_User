db71855a672c789a20d61ef431575e8cf3c4d0fd
root <root@NFrag-Home-Desktop.localdomain>
2021-05-06 17:54:33 -0700

    Updates

---
+++ precache-manifest.e4f08a0ef1346c0c204855a558effcf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7a724891882c2ec52a384b790e06bab",
+    "url": "/React-Random_User/index.html"
+  },
+  {
+    "revision": "f7cf2c0f6ee8944659e4",
+    "url": "/React-Random_User/static/css/main.5921d42c.chunk.css"
+  },
+  {
+    "revision": "5bf2c449ea131aa1d216",
+    "url": "/React-Random_User/static/js/2.a987e3f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Random_User/static/js/2.a987e3f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7cf2c0f6ee8944659e4",
+    "url": "/React-Random_User/static/js/main.58465bd1.chunk.js"
+  },
+  {
+    "revision": "b1c73a93ea9193c27e16",
+    "url": "/React-Random_User/static/js/runtime-main.bd3e1ee6.js"
+  }
+]);
